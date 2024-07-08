73a055390a4b7204dc7f959e540b0cafd3385f86
Jeffery Phipps <jefferyphipps@att.net>
2024-07-08 13:18:25 -0500

    Uploading some chapter examples

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -27,17 +27,18 @@ function sorted(arr){
 }
 //Your function here...
 
-/* BONUS MISSION: Refactor your sorting function to use recursion below:
- /
-/function sortedRec(arr){
+/* BONUS MISSION: Refactor your sorting function to use recursion below:*/
+function sortedRec(arr){
   if (arr[0]===findMinValue(arr)){
-    return;
+    return arr;
   }
   min = findMinValue(arr);
   arr.splice(arr.indexOf(findMinValue(arr)),1);
   console.log(arr);
   return sortedRec(arr);
-}*/
+}
+
+
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -15,3 +15,40 @@ function randomSelection(arr){
  //a) Print 3 random selections from each array.
  //b) Have the code randomly pick one array, and then print 2 random items from it.
  //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+ for(let i = 0; i<3; i++){
+  console.log(randomSelection(happiness)+ " " + randomSelection(words));
+ }
+
+ let index = Math.floor(Math.random()*2);
+ if (index === 0)
+  console.log(randomSelection(happiness)+ " " + randomSelection(happiness));
+else
+  console.log(randomSelection(words)+ " " + randomSelection(words));
+let newArr = [];
+newArr.push(randomSelection(happiness));
+newArr.push(randomSelection(words));
+
+console.log(newArr);
+
+let num = Math.round(Math.random()*10);
+for (let x = 0; x<10;x++)
+  console.log(Math.floor(Math.random()*10));
+
+console.log(Math.round(12.3456789)*100/100);
+
+console.log(Math.round(12.3456789*10000)/10000);
+
+console.log(Math.round(12.3456789)*10000/10000);
+
+console.log(Math.round(12.3456789*100)/100);
+
+let giraffe = {
+  species: "Reticulated Giraffe",
+  name: "Cynthia",
+  weight: 1500,
+  age: 15,
+  diet: "leaves",
+  birthday: function () {return this.age = this.age + 1;}
+};
+
+console.log(giraffe.birthday());
