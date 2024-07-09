3a431cd23ca2c83edb1ed2358b9fc97892857a71
Jeffery Phipps <jefferyphipps@att.net>
2024-07-09 10:58:58 -0500

    Completed bonus missions

--- objects-and-math/studio/ObjectsStudio03.js
+++ objects-and-math/studio/ObjectsStudio03.js
@@ -1,8 +1,25 @@
 // Code your crewMass function here:
-
+function crewMass(arr){
+  let totalMass = 0;
+  for (let x = 0; x<arr.length; x++)
+    totalMass += arr[x].mass;
+  return Math.round(totalMass*10)/10;
+}
 
 // Code your fuelRequired function here:
+function fuelRequired(arr, extraMass = 75000){
+  let fuelNeeded = 0;
+  fuelNeeded = crewMass(arr)+extraMass;
+  for(let x = 0; x<arr.length; x++){
+    if(arr[x].species == 'dog'||arr[x].species == 'cat')
+      fuelNeeded += 200;
+    else
+      fuelNeeded +=100;
+  }
 
+
+  return Math.trunc(Math.ceil(fuelNeeded*9.5));
+}
 
 // The pre-selected crew is in the array at the end of this file.
 // Feel free to add, remove, or switch crew members as you see fit.
@@ -51,4 +68,4 @@ let candidateA = {
  };
  
  let crew = [candidateB,candidateD,candidateF];
- 
+ console.log(`The mission has a launch mass of ${crewMass(crew)+75000} kg and requires ${fuelRequired(crewMass(crew),75000)} kg of fuel.`);
