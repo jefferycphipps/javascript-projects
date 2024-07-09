54da23331fa70e5fac9451901640b58c7308ea80
Jeffery Phipps <jefferyphipps@att.net>
2024-07-08 20:44:14 -0500

    Finished Object Studio problems

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,10 +1,12 @@
+let idNumbers = [291, 414, 503, 599, 796, 890];
 // Code your selectRandomEntry function here:
-
+function selectRandomEntry(id){
+  return id[Math.floor(Math.random()*6)];
+}
 
 // Code your buildCrewArray function here:
+let crewId = [];
 
-
-let idNumbers = [291, 414, 503, 599, 796, 890];
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
@@ -53,3 +55,29 @@ let candidateF = {
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
 // Code your template literal and console.log statements:
+let num = selectRandomEntry(idNumbers);
+crewId.push(num);
+for (let x = 1; x<3;x++){
+    num =  selectRandomEntry(idNumbers);
+    while(crewId.indexOf(num)!=-1){
+      num =  selectRandomEntry(idNumbers);
+    }
+    crewId.push(num);
+}
+//console.log(crewId);
+
+
+function selector(id, ani){
+  let crew = [];
+  for (let x = 0; x<3;x++){
+    for (let i = 0; i< ani.length; i++){
+      if (id[x] == ani[i].astronautID){
+        crew.push(ani[i]);
+      }
+    }
+  }
+  return crew;
+}
+let crewAnimals = selector(crewId, animals);
+//console.log(crewAnimals);
+console.log(`${crewAnimals[0].name}, ${crewAnimals[1].name} and ${crewAnimals[2].name} are going to space!`);
--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,15 +1,28 @@
 // Code your orbitCircumference function here:
-
+function orbitCircumference(radius = 2000){
+  return Math.trunc(Math.PI*2*radius);
+}
 
 // Code your missionDuration function here:
+function missionDuration(numberOfOrbits, orbitRadius = 2000, orbitalSpeed = 28000){
+  
+  return Math.round(((numberOfOrbits*orbitCircumference(orbitRadius))/orbitalSpeed)*100)/100;
 
-
+}
+let numberOfOrbits = 5;
+console.log(`The mission will travel ${numberOfOrbits*orbitCircumference()} km and it will take ${missionDuration(5)} hours to complete.`);
 // Copy/paste your selectRandomEntry function here:
-
+function selectRandomEntry(id){
+  return id[Math.floor(Math.random()*6)];
+}
 
 // Code your oxygenExpended function here:
+function oxygenExpended(candidate, radius = 2000, speed = 28000){
+  let hours = missionDuration(3, radius, speed);
+  return `'${candidate.name} will perform the spacewalk, which will last ${hours} hours and require ${Math.round(candidate.o2Used(hours)*1000)/1000} kg of oxygen.'`;
 
-
+}
+let idNumbers = [291, 414, 503, 599, 796, 890];
 // Candidate data & crew array.
 let candidateA = {
    'name':'Gordon Shumway',
@@ -54,5 +67,37 @@ let candidateA = {
    'astronautID':890
  };
  
- let crew = [candidateA,candidateC,candidateE];
+
+ let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+ let crewId = [];
+ // Code your template literal and console.log statements:
+ let num = selectRandomEntry(idNumbers);
+ crewId.push(num);
+ for (let x = 1; x<3;x++){
+     num =  selectRandomEntry(idNumbers);
+     while(crewId.indexOf(num)!=-1){
+       num =  selectRandomEntry(idNumbers);
+     }
+     crewId.push(num);
+ }
+ //console.log(crewId);
  
+ 
+ function selector(id, ani){
+   let crew = [];
+   for (let x = 0; x<3;x++){
+     for (let i = 0; i< ani.length; i++){
+       if (id[x] == ani[i].astronautID){
+         crew.push(ani[i]);
+       }
+     }
+   }
+   return crew;
+ }
+ let crewAnimals = selector(crewId, animals);
+
+ //let crew = [candidateA,candidateC,candidateE];
+ for (let x = 0; x<crewAnimals.length; x++){
+  
+ console.log(oxygenExpended(crewAnimals[x]));
+ }
