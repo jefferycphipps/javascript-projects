0c70323f7386e86de49edfceb5f7d1f8fd9bf57f
Jeffery Phipps <jefferyphipps@att.net>
2024-07-15 14:22:06 -0500

    10k changes

--- unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
+++ unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
@@ -1,5 +1,16 @@
 describe("transmission processor", function() {
 
    //  TODO: put tests here
- 
+   const processor = require('../processor.js');
+
+   describe("transmission processor", function() {
+
+      test("takes a string and returns an object", function() {
+         let result = processor("9701::<489584872710>");
+         expect(typeof result).toBe("object");
+      });
+
+   });
+
+   
  });
