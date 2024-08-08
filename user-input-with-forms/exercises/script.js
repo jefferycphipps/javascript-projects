72b5245acfd3361783300981f6d610c265d39ff2
Jeffery Phipps <jefferyphipps@att.net>
2024-08-08 14:23:54 -0500

    finished exercies

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,17 @@
 //Code Your Solution Below
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let name = document.getElementById("testName");
+        if(name ===""){
+            alert("Need a test name");
+            event.preventDefault();
+        }
+        let date = document.getElementById("testDate");
+        if(date===""){
+            alert("Please select a date");
+            event.preventDefault();
+        }
+        
+    });
+});
